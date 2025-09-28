da7b73319728fbf01fa72be0741deeb08ecb1450
RhuanArquiles <rhuanarquiles07@gmail.com>
2025-09-28 00:21:16 -0300

    Feature: Create homepage

--- frontend/relacao_retidos/src/app/home/home.component.ts
+++ frontend/relacao_retidos/src/app/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { TopbarComponent } from '../topbar/topbar.component';
+import { RouterLink } from "@angular/router";
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [TopbarComponent, RouterLink, LoginComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  totalFrota = 332;
+  veiculosRetidos = 8;
+  frotaDisponivel = this.totalFrota - this.veiculosRetidos ;
 }
